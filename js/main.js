4acbec4a3ffccb325e1bd790cc2d5f31f18bd10d
Katherine Champagne <kechampagne@gmail.com>
2015-04-05 00:07:30 -0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+angular.module("neon", [])
+
+.controller("neonForm", function($scope, $http) {
+       $scope.neonForm = {};
+       $scope.neonForm.name = "love you";
+
+       $scope.neonForm.designs = [
+            {name:'heart', path:'img/love_heart.svg'},
+            {name:'diamond', path:'img/heart.svg'},
+        ];
+
+        $scope.neonForm.fonts = [
+            {name:'sans-serif', family:'Open Sans', size: '55px'},
+            {name:'cursive', family:'Sacramento', size: '60px'},
+        ];
+
+        $scope.neonForm.colors = [
+            {name:'white', hex:'#ffffff', isActive: true},
+            {name:'blue', hex:'#00FFFF', isActive: false},
+            {name:'pink', hex:'#FF00FF', isActive: false},
+            {name:'yellow', hex:'#FFFF00', isActive: false},
+        ];
+
+        $scope.neonForm.myDesign = $scope.neonForm.designs[0];
+        $scope.neonForm.myFont = $scope.neonForm.fonts[0];
+        $scope.neonForm.myColor = $scope.neonForm.colors[0];
+
+        $scope.neonForm.setMyColor = function(color){
+          console.log($scope.neonForm.colors);
+          console.log(color.name);
+        };
+
+        $scope.neonForm.selectColor = function(index, color) {
+          $scope.selectedColor = index;
+          $scope.neonForm.myColor = color;
+          console.log(color.name);
+        };
+
+        $scope.neonForm.selectFont = function(index, font) {
+          $scope.selectedFont = index;
+          $scope.neonForm.myFont = font;
+          console.log(font.name);
+        };
+
+
+     // $scope.myForm.submitTheForm = function(item, event) {
+     //   console.log("--> Submitting form");
+     //   var dataObject = {
+     //      name : $scope.myForm.name
+     //      ,car  : $scope.myForm.car
+     //   };
+
+     //   var responsePromise = $http.post("/angularjs-examples/json-test-data.jsp", dataObject, {});
+     //   responsePromise.success(function(dataFromServer, status, headers, config) {
+     //      console.log(dataFromServer.title);
+     //   });
+     //    responsePromise.error(function(data, status, headers, config) {
+     //      alert("Submitting form failed!");
+     //   });
+     // }
+  });
